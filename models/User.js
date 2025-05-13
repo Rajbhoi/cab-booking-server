0707327420db7a4c18ceac925475265e8896f4ca
Raj Rajbhoi <rajbhoiraj5@gmail.com>
2025-05-13 21:29:40 +0530

    added reset and forgot password api

--- models/User.js
+++ models/User.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-}, { timestamps: true });
+  password: { type: String, required: true },
+  resetToken: String,
+  resetTokenExpire: Date,
+}, { timestamps: true })
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
